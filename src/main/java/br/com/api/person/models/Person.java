c2dc28e0ca333e242c28cdf30077adfea4bdd106
Luiz Bueno <luizbueno.dev@gmail.com>
2026-05-14 15:40:41 -0300

    remove unused import

--- src/main/java/br/com/api/person/models/Person.java
+++ src/main/java/br/com/api/person/models/Person.java
@@ -1,6 +1,5 @@
 package br.com.api.person.models;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
