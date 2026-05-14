2812985bf82c24077b9423cf0781b0d0a91de74b
Luiz Bueno <luizbueno.dev@gmail.com>
2026-05-14 15:30:51 -0300

    add person model

---
+++ src/main/java/br/com/api/person/models/Person.java
@@ -0,0 +1,22 @@
+package br.com.api.person.models;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String city;
+    private int age;
+}
