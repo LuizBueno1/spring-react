c07e8b775cde767f1a8b0717aac82b5fa13ae9b1
Luiz Bueno <luizbueno.dev@gmail.com>
2026-05-15 17:11:53 -0300

    add register method

--- src/main/java/br/com/api/person/controller/PersonController.java
+++ src/main/java/br/com/api/person/controller/PersonController.java
@@ -1,15 +1,39 @@
 package br.com.api.person.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.person.models.Person;
+import br.com.api.person.service.PersonService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
+@RequestMapping("/persons")
+@RequiredArgsConstructor
 public class PersonController {
+
+    private final PersonService service;
 
     @GetMapping
     public String helloWorld() {
         return "Hello World!";
     }
+
+    @PostMapping
+    public ResponseEntity<Person> register(@RequestBody Person person) {
+
+        Person newPerson = service.registerPerson(person);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPerson);
+
+    }
+    
     
 }
